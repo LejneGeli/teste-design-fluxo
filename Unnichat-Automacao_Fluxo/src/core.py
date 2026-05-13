67917b9ffc661376873152348ff9f22bf5975bfa
LejneGeli <carolgeli34@gmail.com>
2026-05-13 15:33:48 -0300

    Adicionando templates

--- Unnichat-Automacao_Fluxo/src/core.py
+++ Unnichat-Automacao_Fluxo/src/core.py
@@ -189,6 +189,13 @@ TEMPLATES_WHATSAPP = {
             "userId": "3jNt3SinWzW08N7iqGhu473b7M53",
             "id": "2351281792011799"
         },
+        "Fluxo 1 Congresso": {
+            "nome": "m1_segunda_gratuito_congresso_c1",
+            "connectionId": "ouLDZiM3pEddrZuTGJVw",
+            "wabaId": "551316877339100",
+            "userId": "3jNt3SinWzW08N7iqGhu473b7M53",
+            "id": "963935253260325"
+        },
         "Fluxo 2": {
             "nome": "m1_terca_gratuito_curso_c1",
             "connectionId": "ouLDZiM3pEddrZuTGJVw",
@@ -196,6 +203,13 @@ TEMPLATES_WHATSAPP = {
             "userId": "3jNt3SinWzW08N7iqGhu473b7M53",
             "id": "1224032236224575"
         },
+        "Fluxo 2 Congresso": {
+            "nome": "m1_terca_gratuito_congresso_c1",
+            "connectionId": "ouLDZiM3pEddrZuTGJVw",
+            "wabaId": "551316877339100",
+            "userId": "3jNt3SinWzW08N7iqGhu473b7M53",
+            "id": "2181188712698535"
+        },       
         "Fluxo 7": {
             "nome": "m1_quarta_gratuito_curso_c1",
             "connectionId": "ouLDZiM3pEddrZuTGJVw",
@@ -203,13 +217,27 @@ TEMPLATES_WHATSAPP = {
             "userId": "3jNt3SinWzW08N7iqGhu473b7M53" ,
             "id": "3877399019065544"
         }, 
+        "Fluxo 7 Congresso": {
+            "nome": "m1_quarta_gratuito_congresso_c1",
+            "connectionId": "ouLDZiM3pEddrZuTGJVw",
+            "wabaId": "551316877339100",
+            "userId": "3jNt3SinWzW08N7iqGhu473b7M53",
+            "id": "1663317388284499"
+        },
         "SC1": {
             "nome": "m1_terca_sc1_curso_c1",
             "connectionId": "ouLDZiM3pEddrZuTGJVw",
             "wabaId": "551316877339100",
             "userId": "3jNt3SinWzW08N7iqGhu473b7M53",
             "id": "2204131727075346"
-        }, 
+        },
+        "SC1 Congresso": {
+            "nome": "m1_terca_sc1_congresso_c1",
+            "connectionId": "ouLDZiM3pEddrZuTGJVw",
+            "wabaId": "551316877339100",
+            "userId": "3jNt3SinWzW08N7iqGhu473b7M53",
+            "id": "2311199216071077"
+        },
         "SC2":{
             "nome": "m1_quinta_sc2_curso_c1",
             "connectionId": "ouLDZiM3pEddrZuTGJVw",
@@ -217,12 +245,26 @@ TEMPLATES_WHATSAPP = {
             "userId": "3jNt3SinWzW08N7iqGhu473b7M53" ,
             "id": "939470922406268"
         }, 
+        "SC2 Congresso": {
+            "nome": "m1_quinta_sc2_congresso_c1",
+            "connectionId": "ouLDZiM3pEddrZuTGJVw",
+            "wabaId": "551316877339100",
+            "userId": "3jNt3SinWzW08N7iqGhu473b7M53",
+            "id": "1908162669901036"
+        },
         "SC3": {
             "nome": "m1_quinta_sc3_curso_c1",
             "connectionId": "ouLDZiM3pEddrZuTGJVw" ,
             "wabaId": "551316877339100",
             "userId": "3jNt3SinWzW08N7iqGhu473b7M53",
             "id": "1263805545421820"
+        },
+        "SC3 Congresso": {
+            "nome": "m1_quinta_sc3_congresso_c1",
+            "connectionId": "ouLDZiM3pEddrZuTGJVw",
+            "wabaId": "551316877339100",
+            "userId": "3jNt3SinWzW08N7iqGhu473b7M53",
+            "id": "1515771696739423"
         }
     },
     "Conta_2": {
@@ -233,6 +275,13 @@ TEMPLATES_WHATSAPP = {
             "userId": "3jNt3SinWzW08N7iqGhu473b7M53",
             "id": "1543760574427026"
         },
+        "Fluxo 1 Congresso": {
+            "nome": "m1_segunda_gratuito_congresso_c2",
+            "connectionId": "OXU0Zg19Qgu5G4HauCdC",
+            "wabaId": "643489112180162",
+            "userId": "3jNt3SinWzW08N7iqGhu473b7M53",
+            "id": "1984470522461741"
+        },
         "Fluxo 2": {
             "nome": "m1_terca_gratuito_curso_c2",
             "connectionId": "OXU0Zg19Qgu5G4HauCdC",
@@ -240,13 +289,27 @@ TEMPLATES_WHATSAPP = {
             "userId": "3jNt3SinWzW08N7iqGhu473b7M53",
             "id": "4432723350280208"
         },
+        "Fluxo 2 Congresso": {
+            "nome": "m1_terca_gratuito_congresso_c2",
+            "connectionId": "OXU0Zg19Qgu5G4HauCdC",
+            "wabaId": "643489112180162",
+            "userId": "3jNt3SinWzW08N7iqGhu473b7M53",
+            "id": "823186110287063"
+        },
         "Fluxo 7": {
             "nome": "m1_quarta_gratuito_curso_c2",
             "connectionId": "OXU0Zg19Qgu5G4HauCdC",
             "wabaId": "643489112180162",
             "userId": "3jNt3SinWzW08N7iqGhu473b7M53" ,
             "id": "26688921900744732"
-        }, 
+        },
+        "Fluxo 7 Congresso": {
+            "nome": "m1_quarta_gratuito_congresso_c2",
+            "connectionId": "OXU0Zg19Qgu5G4HauCdC",
+            "wabaId": "643489112180162",
+            "userId": "3jNt3SinWzW08N7iqGhu473b7M53",
+            "id": "1876987939624672"
+        },
         "SC1": {
             "nome": "m1_terca_sc1_curso_c2",
             "connectionId": "OXU0Zg19Qgu5G4HauCdC",
@@ -254,6 +317,13 @@ TEMPLATES_WHATSAPP = {
             "userId": "3jNt3SinWzW08N7iqGhu473b7M53",
             "id": "4404558556458070"
         }, 
+        "SC1 Congresso": {
+            "nome": "m1_terca_sc1_congresso_c2",
+            "connectionId": "OXU0Zg19Qgu5G4HauCdC",
+            "wabaId": "643489112180162",
+            "userId": "3jNt3SinWzW08N7iqGhu473b7M53",
+            "id": "2117207822462821"
+        },
         "SC2":{
             "nome": "m1_quinta_sc2_curso_c2",
             "connectionId": "OXU0Zg19Qgu5G4HauCdC",
@@ -261,13 +331,27 @@ TEMPLATES_WHATSAPP = {
             "userId": "3jNt3SinWzW08N7iqGhu473b7M53" ,
             "id": "1561244302678164"
         }, 
+        "SC2 Congresso": {
+            "nome": "m1_quinta_sc2_congresso_c2",
+            "connectionId": "OXU0Zg19Qgu5G4HauCdC",
+            "wabaId": "643489112180162",
+            "userId": "3jNt3SinWzW08N7iqGhu473b7M53",
+            "id": "2183233749095726"
+        },
         "SC3": {
             "nome": "m1_quinta_sc3_curso_c2",
             "connectionId": "OXU0Zg19Qgu5G4HauCdC" ,
             "wabaId": "643489112180162",
             "userId": "3jNt3SinWzW08N7iqGhu473b7M53",
             "id": "1351355953500204" 
-        } 
+        },
+        "SC3 Congresso": {
+            "nome": "m1_quinta_sc3_congresso_c2",
+            "connectionId": "OXU0Zg19Qgu5G4HauCdC",
+            "wabaId": "643489112180162",
+            "userId": "3jNt3SinWzW08N7iqGhu473b7M53",
+            "id": "945328344776651"
+        }
     },
     "Conta_3": {
             "Fluxo 1": {
@@ -277,12 +361,26 @@ TEMPLATES_WHATSAPP = {
             "userId": "3jNt3SinWzW08N7iqGhu473b7M53",
             "id": "2853631534991765"
         },
+        "Fluxo 1 Congresso": {
+            "nome": "m1_segunda_gratuito_congresso_c3",
+            "connectionId": "KeL8BkXcV3WWwW6j7MHc",
+            "wabaId": "1488880795714059",
+            "userId": "3jNt3SinWzW08N7iqGhu473b7M53",
+            "id": "1920212902714412"
+        },
         "Fluxo 2": {
             "nome": "m1_terca_gratuito_curso_c3",
             "connectionId": "KeL8BkXcV3WWwW6j7MHc",
             "wabaId": "1488880795714059",
             "userId": "3jNt3SinWzW08N7iqGhu473b7M53",
             "id": "823556080800952"
+        },
+        "Fluxo 2 Congresso": {
+            "nome": "m1_terca_gratuito_congresso_c3",
+            "connectionId": "KeL8BkXcV3WWwW6j7MHc",
+            "wabaId": "1488880795714059",
+            "userId": "3jNt3SinWzW08N7iqGhu473b7M53",
+            "id": "1338801454803295"
         },
         "Fluxo 7": {
             "nome": "m1_quarta_gratuito_curso_c3",
@@ -291,6 +389,13 @@ TEMPLATES_WHATSAPP = {
             "userId": "3jNt3SinWzW08N7iqGhu473b7M53" ,
             "id": "1267302135598246"
         }, 
+        "Fluxo 7 Congresso": {
+            "nome": "m1_quarta_gratuito_congresso_c3",
+            "connectionId": "KeL8BkXcV3WWwW6j7MHc",
+            "wabaId": "1488880795714059",
+            "userId": "3jNt3SinWzW08N7iqGhu473b7M53",
+            "id": "1267302135598246"
+        },
         "SC1": {
             "nome": "m1_terca_sc1_curso_c3",
             "connectionId": "KeL8BkXcV3WWwW6j7MHc",
@@ -298,6 +403,13 @@ TEMPLATES_WHATSAPP = {
             "userId": "3jNt3SinWzW08N7iqGhu473b7M53",
             "id": "1455864015527736"
         }, 
+        "SC1 Congresso": {
+            "nome": "m1_terca_sc1_congresso_c3",
+            "connectionId": "KeL8BkXcV3WWwW6j7MHc",
+            "wabaId": "1488880795714059",
+            "userId": "3jNt3SinWzW08N7iqGhu473b7M53",
+            "id": "1647824836263779"
+        },
         "SC2":{
             "nome": "m1_quinta_sc2_curso_c3",
             "connectionId": "KeL8BkXcV3WWwW6j7MHc",
@@ -305,12 +417,26 @@ TEMPLATES_WHATSAPP = {
             "userId": "3jNt3SinWzW08N7iqGhu473b7M53" ,
             "id": "3517092801775768"
         }, 
+        "SC2 Congresso": {
+            "nome": "m1_quinta_sc2_congresso_c3",
+            "connectionId": "KeL8BkXcV3WWwW6j7MHc",
+            "wabaId": "1488880795714059",
+            "userId": "3jNt3SinWzW08N7iqGhu473b7M53",
+            "id": "1683629602827188"
+        },
         "SC3": {
             "nome": "m1_quinta_sc3_congresso_c3",
             "connectionId": "KeL8BkXcV3WWwW6j7MHc" ,
             "wabaId": "1488880795714059",
             "userId": "3jNt3SinWzW08N7iqGhu473b7M53",
             "id": "1494247202091960" 
+        },
+        "SC3 Congresso": {
+            "nome": "m1_quinta_sc3_congresso_c3",
+            "connectionId": "KeL8BkXcV3WWwW6j7MHc",
+            "wabaId": "1488880795714059",
+            "userId": "3jNt3SinWzW08N7iqGhu473b7M53",
+            "id": "890898067333698"
         }
     },
     "Conta_4": {
@@ -321,12 +447,26 @@ TEMPLATES_WHATSAPP = {
             "userId": "3jNt3SinWzW08N7iqGhu473b7M53",
             "id": "2853631534991765"
         },
+        "Fluxo 1 Congresso": {
+            "nome": "m1_segunda_gratuito_congresso_c4",
+            "connectionId": "tOgoXMt8I9a21Pj1BiJx",
+            "wabaId": "4237462246578095",
+            "userId": "3jNt3SinWzW08N7iqGhu473b7M53",
+            "id": "1503338854734344"
+        },
         "Fluxo 2": {
             "nome": "m1_terca_gratuito_curso_c4",
             "connectionId": "tOgoXMt8I9a21Pj1BiJx",
             "wabaId": "4237462246578095",
             "userId": "3jNt3SinWzW08N7iqGhu473b7M53",
             "id": "1671339140859416"
+        },
+        "Fluxo 2 Congresso": {
+            "nome": "m1_terca_gratuito_congresso_c4",
+            "connectionId": "tOgoXMt8I9a21Pj1BiJx",
+            "wabaId": "4237462246578095",
+            "userId": "3jNt3SinWzW08N7iqGhu473b7M53",
+            "id": "1271267434584841"
         },
         "Fluxo 7": {
             "nome": "m1_quarta_gratuito_curso_c4",
@@ -335,6 +475,13 @@ TEMPLATES_WHATSAPP = {
             "userId": "3jNt3SinWzW08N7iqGhu473b7M53" ,
             "id": "3096299253905186"
         }, 
+        "Fluxo 7 Congresso": {
+            "nome": "m1_quarta_gratuito_congresso_c4",
+            "connectionId": "tOgoXMt8I9a21Pj1BiJx",
+            "wabaId": "4237462246578095",
+            "userId": "3jNt3SinWzW08N7iqGhu473b7M53",
+            "id": "1636155757658988"
+        },
         "SC1": {
             "nome": "m1_terca_sc1_curso_c4",
             "connectionId": "tOgoXMt8I9a21Pj1BiJx",
@@ -342,6 +489,13 @@ TEMPLATES_WHATSAPP = {
             "userId": "3jNt3SinWzW08N7iqGhu473b7M53",
             "id": "1956234691946131"
         }, 
+        "SC1 Congresso": {
+            "nome": "m1_terca_sc1_congresso_c4",
+            "connectionId": "tOgoXMt8I9a21Pj1BiJx",
+            "wabaId": "4237462246578095",
+            "userId": "3jNt3SinWzW08N7iqGhu473b7M53",
+            "id": "1295130972804753"
+        },
         "SC2":{
             "nome": "m1_quinta_sc2_curso_c4",
             "connectionId": "tOgoXMt8I9a21Pj1BiJx",
@@ -349,12 +503,26 @@ TEMPLATES_WHATSAPP = {
             "userId": "3jNt3SinWzW08N7iqGhu473b7M53" ,
             "id": "1265321088710437"
         }, 
+        "SC2 Congresso": {
+            "nome": "m1_quinta_sc2_congresso_c4",
+            "connectionId": "tOgoXMt8I9a21Pj1BiJx",
+            "wabaId": "4237462246578095",
+            "userId": "3jNt3SinWzW08N7iqGhu473b7M53",
+            "id": "1355314149773137"
+        },
         "SC3": {
             "nome": "m1_quinta_sc3_curso_c4",
             "connectionId": "tOgoXMt8I9a21Pj1BiJx" ,
             "wabaId": "4237462246578095",
             "userId": "3jNt3SinWzW08N7iqGhu473b7M53",
             "id": "828526586972488"
+        },
+        "SC3 Congresso": {
+            "nome": "m1_quinta_sc3_congresso_c4",
+            "connectionId": "tOgoXMt8I9a21Pj1BiJx",
+            "wabaId": "4237462246578095",
+            "userId": "3jNt3SinWzW08N7iqGhu473b7M53",
+            "id": "1459372082386644"
         }
     },
 }
